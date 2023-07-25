8ee0525002e73e47b439efbf4e21db0764910192
Andrés Correa Casablanca <castarco@coderspirit.xyz>
2023-07-25 18:52:23 +0200

    chore: minimal changes

--- src/beautiful-tree.tsx
+++ src/beautiful-tree.tsx
@@ -7,9 +7,21 @@ export type Tree = {
 }
   
 export interface BeautifulTreeProps {
-    tree: Tree    
+    id: string,
+    svgProps: {
+        width: number,
+        height: number,
+    },
+    tree: Tree,
 }
 
-export const BeautifulTree = (props: BeautifulTreeProps) => {
-    console.log(props)
+export function BeautifulTree(props: BeautifulTreeProps) {
+    return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+            id={props.id}
+			viewBox={`0 0 ${props.svgProps.width} ${props.svgProps.height}`}
+			className={'beautiful-tree-react'}
+		></svg>
+    )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react'],
+      external: ['react', 'react-dom'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
