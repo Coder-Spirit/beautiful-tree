fff110531a683b701bee5018ef66e16c226600d5
Andrés Correa Casablanca <castarco@coderspirit.xyz>
2023-07-27 12:47:10 +0200

    refactor: use rollup instead of vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2020: true
+	root: true,
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+    ecmaVersion: 2022,
+		sourceType: 'module',
+    tsConfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-  extends: [
-    'eslint:recommended',
-    `plugin:@typescript-eslint/strict-type-checked`,
-    'plugin:react-hooks/recommended',
-    'plugin:storybook/recommended',
-    `plugin:react/recommended`,
-    `plugin:react/jsx-runtime`
-  ],
-  settings: {
+	plugins: [
+		'@typescript-eslint',
+		'prettier',
+		'react-refresh'
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	settings: {
     react: {
       version: 'detect'
     },
   },
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', {
-      allowConstantExport: true
-    }]
-  }
-};
+	ignorePatterns: [
+		'.eslintrc.cjs',
+		'dist/**/*',
+		'node_modules/**/*',
+	]
+}
